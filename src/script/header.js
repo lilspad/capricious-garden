b26a074f988a72156bdb93b0b3f89b19a17d0d54
lilspad <lilypaczesniak@outlook.com>
2023-01-30 01:29:25 +0000

    Update header.js

--- src/script/header.js
+++ src/script/header.js
@@ -11,8 +11,9 @@ menu.classList.add('menu')
 
 menu.innerHTML = '<i class="fa-solid fa-bars" /> <nav class="header-nav" ><a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/plants/plants.html">PLANTS</a>' + 
 '<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/supplies/supplies.html">SUPPLIES</a>' + 
-'<a>CLASSES</a>' + 
-'<a>CONTACT</a><a>ABOUT US</a>' +
+'<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/classes.html" >CLASSES</a>' + 
+'<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/contact.html">CONTACT</a>' + 
+'<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/about.html">ABOUT US</a>' +
 '<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/supplies/partners.html">PARTNERS</a></nav>';
 
 document.getElementById('header').appendChild(logo);
