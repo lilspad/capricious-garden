00420db55a1943b0d8d2f465dc0cf6a7fa6c699a
Lily Paczesniak <lilypaczesniak@outlook.com>
2023-02-03 02:28:54 +0000

    Update header.js

--- src/script/header.js
+++ src/script/header.js
@@ -9,12 +9,12 @@ logo.innerHTML = '<i class="fa-brands fa-pagelines" /> <h2>CAPRICIOUS</h2>';
 var menu = document.createElement('div');
 menu.classList.add('menu')
 
-menu.innerHTML = '<i class="fa-solid fa-bars" /> <nav class="header-nav" ><a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/plants/plants.html">PLANTS</a>' + 
-'<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/supplies/supplies.html">SUPPLIES</a>' + 
-'<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/classes.html" >CLASSES</a>' + 
-'<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/contact.html">CONTACT</a>' + 
-'<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/about.html">ABOUT US</a>' +
-'<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/supplies/partners.html">PARTNERS</a></nav>';
+menu.innerHTML = '<i class="fa-solid fa-bars" /> <nav class="header-nav" ><a href="/src/html/plants/plants.html">PLANTS</a>' + 
+'<a href="/src/html/supplies/supplies.html">SUPPLIES</a>' + 
+'<a href="/src/html/classes.html" >CLASSES</a>' + 
+'<a href="/src/html/contact.html">CONTACT</a>' + 
+'<a href="/src/html/about.html">ABOUT US</a>' +
+'<a href="/src/html/supplies/partners.html">PARTNERS</a></nav>';
 
 document.getElementById('header').appendChild(logo);
 document.getElementById('header').appendChild(menu);
