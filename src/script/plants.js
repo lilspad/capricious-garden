1de9b1b85d65d311b59c769c80d406e7985e59c0
lilspad <lilypaczesniak@outlook.com>
2023-02-03 02:07:49 +0000

    filter functionality

--- src/script/plants.js
+++ src/script/plants.js
@@ -1,21 +1,81 @@
+var display = document.getElementById('plants-display');
+var filters = document.getElementsByClassName('filter');
+var filtersDiv = document.getElementsByClassName('filter-div');
+var types = document.getElementsByClassName('plant-type');
+
 var foliage = ["alocasia", "begonia", "calathea", "chinese evergreen", "croton", "ferns", "fittonia", "indoor trees", "ivy", "maranta", 
 "palms", "philodendron", "pothos", "spider plant"];
 var succulents = ["agave", "aloe", "cacti", "calisia", "ceropegia", "crassula", "echeveria", "gasteria", "haworthia", "hoya", "kalanchoe", "peporomia", 
-"sedum", "senecio", "tradescantia", "yucca"]
+"sedum", "senecio", "tradescantia", "yucca"];
 
-var flowering = ["african violet", "bromeliad", "clivia", "crown-of-thorns", "geranium", "kalanchoe", "maracas", "orchids"]
+var flowering = ["african violet", "bromeliad", "clivia", "crown-of-thorns", "geranium", "kalanchoe", "maracas", "orchids"];
 
 var trees = ["citrus", "dracaena", "ficus", "laurels", "pachira", "pine", "yucca"];
 var ferns = ["asparagus", "bird's nest", "blue star", "boston", "maidenhair", "mother", "staghorn"];
 
-var indoor = [foliage, succulents, flowering]
-
-function createPlant(name, enviroment, type, sizes, priceFrom) {
+/*function createPlant(name, enviroment, type, sizes, pricing) {
     return {
         name: name,
         enviroment: enviroment,
         type: type,
         sizes: sizes,
-        priceFrom: priceFrom
+        pricing: pricing
+    }
+}*/
+
+var indoor = [foliage, succulents, flowering];
+var easy = ['some', 'easy', 'to care for', 'plants'];
+var fussy = ['some', 'plants', 'that', 'are fussy'];
+
+
+
+
+let currentFilter = indoor;
+
+const getCurrentFilter = () => {
+    if (sessionStorage.getItem('currentFilter')) {
+        currentFilter = sessionStorage.getItem('currentFilter');
+        return currentFilter;
+    } else {
+        sessionStorage.setItem('currentFilter', currentFilter);
+        return currentFilter;
     }
 }
+
+display.innerHTML = getCurrentFilter();
+
+for (let i = 0; i < filters.length; i++) {
+    filters[i].addEventListener('click', () => {
+
+        for (let j = 0; j < filters.length; j++) {
+            filtersDiv[j].style.backgroundColor = 'inherit';
+            filtersDiv[j].style.color = 'black';
+        }
+
+        filtersDiv[i].style.backgroundColor = '#501537';
+        filtersDiv[i].style.color = 'white';
+
+        switch(filters[i].innerText) {
+            case 'ALL':
+                currentFilter = indoor;
+                break;
+            case 'EASYCARE':
+                currentFilter = easy;
+                break;
+            case 'FUSSY':
+                currentFilter = fussy;
+                break;
+            case 'FLOWERING':
+                currentFilter = flowering;
+                break;
+            case 'FOLIAGE':
+                currentFilter = foliage;
+                break;
+            case 'SUCCULENTS':
+                currentFilter = succulents;
+        }
+
+        sessionStorage.setItem('currentFilter', currentFilter);
+        display.innerHTML = getCurrentFilter();
+    })
+};
