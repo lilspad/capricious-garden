52031869a189a6d3763477dda43668a8606b3e60
Lily Paczesniak <lilypaczesniak@outlook.com>
2023-02-09 20:18:33 +0000

    Update header.js

--- src/script/header.js
+++ src/script/header.js
@@ -9,12 +9,12 @@ logo.innerHTML = '<i class="fa-brands fa-pagelines" /> <h2>CAPRICIOUS</h2>';
 var menu = document.createElement('div');
 menu.classList.add('menu')
 
-menu.innerHTML = '<i class="fa-solid fa-bars" /> <nav class="header-nav" ><a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/plants/plants.html">PLANTS</a>' + 
-'<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/supplies/supplies.html">SUPPLIES</a>' + 
-'<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/classes.html" >CLASSES</a>' + 
-'<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/contact.html">CONTACT</a>' + 
-'<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/about.html">ABOUT US</a>' +
-'<a href="/Users/lilspad/Desktop/Projects/for-real/capricious-garden/src/html/supplies/partners.html">PARTNERS</a></nav>';
+menu.innerHTML = '<i class="fa-solid fa-bars" /> <nav class="header-nav" ><a href="https://capricious.netlify.app/src/html/plants/plants.html">PLANTS</a>' + 
+'<a href="https://capricious.netlify.app/src/html/supplies/supplies.html">SUPPLIES</a>' + 
+'<a href="https://capricious.netlify.app/src/html/classes.html" >CLASSES</a>' + 
+'<a href="https://capricious.netlify.app/src/html/contact.html">CONTACT</a>' + 
+'<a href="https://capricious.netlify.app/src/html/about.html">ABOUT US</a>' +
+'<a href="https://capricious.netlify.app/src/html/supplies/partners.html">PARTNERS</a></nav>';
 
 document.getElementById('header').appendChild(logo);
 document.getElementById('header').appendChild(menu);
