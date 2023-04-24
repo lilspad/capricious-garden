52326736051b16499dbbcc94dadc6281a4c637b7
lilspad <lilypaczesniak@outlook.com>
2023-04-24 22:41:40 +0100

    add command line script

--- src/script/data/plants.sql
+++ src/script/data/plants.sql
@@ -1,3 +1,27 @@
+-- terminal
+/* .schema plants
+CREATE TABLE plants(
+	id INTEGER PRIMARY KEY,
+	img TEXT,
+	scientific_name TEXT,
+	common_name TEXT,
+	type TEXT,
+	care_level TEXT,
+	price INTEGER
+);
+DROP TABLE IF EXISTS plants;
+CREATE TABLE plants(
+	id INTEGER PRIMARY KEY NOT NULL,
+	img TEXT,
+	scientific_name TEXT NOT NULL,
+	common_name TEXT NOT NULL,
+	type TEXT NOT NULL,
+	care_level TEXT NOT NULL,
+	price INTEGER NOT NULL
+);
+.mode csv
+.import plants-data-no-header.csv plants */
+
 --select all plants:
 SELECT * FROM plants;
 
