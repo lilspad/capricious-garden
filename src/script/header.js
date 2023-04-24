c8723777272f186de754af452b00b5dc35e833e7
lilspad <lilypaczesniak@outlook.com>
2023-04-24 23:34:53 +0100

    Update header.js

--- src/script/header.js
+++ src/script/header.js
@@ -10,11 +10,10 @@ var menu = document.createElement('div');
 menu.classList.add('menu')
 
 menu.innerHTML = '<i class="fa-solid fa-bars" /> <nav class="header-nav" ><a href="https://capricious.netlify.app/src/html/plants/plants.html">PLANTS</a>' + 
-'<a href="https://capricious.netlify.app/src/html/supplies/supplies.html">SUPPLIES</a>' + 
+'<a href="https://capricious.netlify.app/src/html/supplies/substrate.html">SUPPLIES</a>' + 
 '<a href="https://capricious.netlify.app/src/html/classes.html" >CLASSES</a>' + 
 '<a href="https://capricious.netlify.app/src/html/contact.html">CONTACT</a>' + 
-'<a href="https://capricious.netlify.app/src/html/about.html">ABOUT US</a>' +
-'<a href="https://capricious.netlify.app/src/html/supplies/partners.html">PARTNERS</a></nav>';
+'<a href="https://capricious.netlify.app/src/html/about.html">ABOUT US</a>';
 
 document.getElementById('header').appendChild(logo);
 document.getElementById('header').appendChild(menu);
